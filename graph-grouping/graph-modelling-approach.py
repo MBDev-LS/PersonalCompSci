941e50c4c55cf010049192871dcdc1be5aac6ad5
Louis Stevens <louis@louisstevens.com>
2023-07-14 19:21:55 +0100

    Adds function setup required to write the grouping algorithms.

--- graph-grouping/graph-modelling-approach.py
+++ graph-grouping/graph-modelling-approach.py
@@ -37,21 +37,44 @@ def check_graph_for_cycles(adjacencyTable: dict) -> bool:
 		return False
 
 
+def split_by_bfs(adjacencyTable: dict, groupSize: int) -> list:
+	pass
 
-print(check_graph_for_cycles(TEST_GRAPH_DICT))
 
-def check_graph_for_cycles2(adjacencyTable: dict) -> bool:
-	if len(adjacencyTable) == 0:
-		return False
+def get_splits_by_dfs(adjacencyTable: dict, groupSize: int, currentNode: str, visited: set=None) -> list:
+	if visited == None:
+		visited = set()
 
-	nodesWithOneEdge = [
-		node for node in adjacencyTable if len(adjacencyTable[node]) == 1
-	]
 
+def get_groups_of_node_groups_of_n(adjacencyTable: dict, n: int) -> list:
+	graphHasCycles = check_graph_for_cycles(TEST_GRAPH_DICT)
 	
-	if len(nodesWithOneEdge) > 0:
-		cyclesInGraph = dfs_cycles_check(adjacencyTable, nodesWithOneEdge[0]) == True
+	if len(adjacencyTable) % n != 0:
+		raise ValueError('The number of nodes in adjacencyTable must be a multiple of n.')
+
+	if graphHasCycles is True:
+		return [split_by_bfs(adjacencyTable, n)]
 	else:
-		cyclesInGraph = dfs_cycles_check(adjacencyTable, adjacencyTable[0]) == True
+		nodesWithOneEdge = [
+			node for node in adjacencyTable if len(adjacencyTable[node]) == 1
+		]
+
+		return get_splits_by_dfs(adjacencyTable, n, )
+
+
+
+	# nodesWithOneEdge = [
+	# 	node for node in adjacencyTable if len(adjacencyTable[node]) == 1
+	# ]
+
 	
-	return cyclesInGraph
+	# if len(nodesWithOneEdge) > 0:
+	# 	cyclesInGraph = dfs_cycles_check(adjacencyTable, nodesWithOneEdge[0]) == True
+	# else:
+	# 	cyclesInGraph = dfs_cycles_check(adjacencyTable, adjacencyTable[0]) == True
+	
+	# return cyclesInGraph
+
+
+
+groups_of_node_groups = get_groups_of_node_groups_of_n(TEST_GRAPH_DICT, 3)
