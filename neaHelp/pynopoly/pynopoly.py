69b970ce13751256024ac3bb4bc755f61f236643
Louis <louis@louisstevens.com>
2023-12-11 17:40:53 +0000

    Adds complete signup process and database setup.

--- neaHelp/pynopoly/pynopoly.py
+++ neaHelp/pynopoly/pynopoly.py
@@ -3,27 +3,7 @@ import config
 
 import sqlite3
 
-
-
-con = sqlite3.connect(config.DATABASE_DIR)
-cur = con.cursor()
-
-
-cur.execute('''
-	CREATE TABLE IF NOT EXISTS players(
-			playerId 		INT 			NOT NULL,
-			playerUsername 	VARCHAR(25) 	NOT NULL,
-			playerName 		VARCHAR(50) 	NOT NULL,
-			playerPin 		VARCHAR(6) 		NOT NULL,
-			playerWinCount		INT,
-
-			PRIMARY KEY (playerId, playerUsername)
-		)
-''')
-
-
-
-print(config.BASE_DIR)
+import re
 
 class Player():
 	def __init__(self):
@@ -32,6 +12,7 @@ class Player():
 
 		self.name = None
 		self.username = None
+		self.playerId = None
 
 	@classmethod
 	def getYesNoInput(cls, prompt: str) -> bool:
@@ -48,13 +29,59 @@ class Player():
 	def validatePlayerPin(cls, playerPin: str) -> bool:
 		return len(playerPin) >= 4 and len(playerPin) <= 6 and playerPin.isdigit()
 	
+	@classmethod
+	def checkUsernameIsUnique(cls, playerUsername: str) -> bool:
+
+		usernameLookupCon = sqlite3.connect(config.DATABASE_DIR)
+		usernameLookupCur = usernameLookupCon.cursor()
+
+		queryResult = usernameLookupCur.execute(
+			'SELECT * FROM Players WHERE playerUsername = :username', 
+			{'username': playerUsername}
+		)
+
+		return len(queryResult.fetchall()) == 0
+
+	@classmethod
+	def validateUsername(cls, playerUsername: str) -> dict:
+		
+		if len(playerUsername) == 0:
+			return {'valid': False, 'error': 'No username provided.'}
+		elif len(playerUsername) > 25:
+			return {'valid': False, 'error': 'Username exceeds maximum length (25).'}
+		elif re.match(r'^[a-zA-Z0-9\-\_\.]{1,25}$', playerUsername) == None:
+			return {'valid': False, 'error': 'Username contains prohibited character.'}
+		
+		return {'valid': True, 'error': None}
+	
+	@classmethod
+	def getValidUsername(cls) -> str:
+		usernameIsUnique = False
+
+		while usernameIsUnique == False:
+			newPlayerUsername = input('Please enter your username (Max length 50, may contain letters, numbers and _ - .): ')
+			validUsernameDict = Player.validateUsername(newPlayerUsername)
+
+			while validUsernameDict['valid'] == False:
+				print(f'\nError: {validUsernameDict['error']}')
+				newPlayerUsername = input('Please enter your username (Max length 50, may contain letters, numbers and _ - .): ')
+				validUsernameDict = Player.validateUsername(newPlayerUsername)
+			
+			if Player.checkUsernameIsUnique(newPlayerUsername) == True:
+				break
+			else:
+				print('Error: Someone is already using this username, please pick a different.')
+		
+		return newPlayerUsername
 
 	def loginPlayer(self):
 		pass
 
 	def signupPlayer(self):
-		newPlayerName = input('Please enter your name: ')
-		newPlayerUsername = input('Please enter your username: ')
+		newPlayerName = input('Please enter your name (Max length 50): ')
+		
+		newPlayerUsername = Player.getValidUsername()
+		
 
 		newPlayerPin = input('Please enter your security pin (it must be an integer with 4 to 6 digits, both inclusive): ')
 		while Player.validatePlayerPin(newPlayerPin) is False:
@@ -64,6 +91,14 @@ class Player():
 		signupCon = sqlite3.connect(config.DATABASE_DIR)
 		signupCur = signupCon.cursor()
 
+		signupCur.execute(f'''
+		INSERT INTO Players (playerId, playerUsername, playerName, playerPin, playerWinCount)
+		VALUES (NULL, ?, ?, ?, ?);
+		''', [newPlayerUsername, newPlayerName, newPlayerPin, 0])
+
+		signupCon.commit()
+
+		signupCur.close()
 
 
 	def loadPlayerProfile(self):
@@ -71,3 +106,37 @@ class Player():
 
 		if playerHasAccount is True:
 			self.loginPlayer()
+		else:
+			self.signupPlayer()
+
+
+player = Player()
+Player.validateUsername('mb')
+Player.validateUsername('mblss')
+player.loadPlayerProfile()
+
+
+def databaseSetup():
+	con = sqlite3.connect(config.DATABASE_DIR)
+	cur = con.cursor()
+
+	cur.execute('''
+		CREATE TABLE IF NOT EXISTS Players(
+				playerId 		INTEGER 		PRIMARY KEY,
+				playerUsername 	VARCHAR(25) 	NOT NULL,
+				playerName 		VARCHAR(50) 	NOT NULL,
+				playerPin 		VARCHAR(6) 		NOT NULL,
+				playerWinCount		INT
+			)
+	''')
+
+	#PRIMARY KEY (playerUsername) - Removed
+
+
+def main():
+	databaseSetup()
+
+
+if __name__ == '__main__':
+	main()
+
